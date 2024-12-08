f17aba39f4d228257d9157706da91380241da55c
faysalahmed3859 <faysalahamed3859@gmail.com>
2024-12-08 23:59:44 +0600

    get put delete method add

--- Program.cs
+++ Program.cs
@@ -4,4 +4,25 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.MapGet("/",() =>
+{
+    return "Api is now work";
+});
+app.MapGet("/hello",() =>
+{
+    return "Get method From: Hello";
+});
+app.MapPost("/hello",() =>
+{
+    return "POST method From: Hello";
+});
+app.MapPut("/hello",() =>
+{
+    return "PUT method From: Hello";
+});
+app.MapDelete("/hello",() =>
+{
+    return "Delete method From: Hello";
+});
+
 app.Run();
